76c7433dc8ac934bcef0a63ff36458418274fe6e
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-01-04 17:58:05 -0600

    pseudocoding complete in js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Need to set a timer of about 90 seconds
+// Players will click start which will begin timer
+// Players will have 90 seconds to choose answers to a variety of questions
+// Show how much time players have to complete the trivia
+// Players can only choose one answer per question
+// At the end of the timer, players will be given results of unanswered, correct and wrong 
